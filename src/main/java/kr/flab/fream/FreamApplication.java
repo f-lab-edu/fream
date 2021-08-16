85822037cd66102c505e7ca25b8d0ad8443a3998
Jake <cocahack@naver.com>
2021-08-16 16:54:40 +0900

    feat: Spring boot 프로젝트 생성

---
+++ src/main/java/kr/flab/fream/FreamApplication.java
@@ -0,0 +1,13 @@
+package kr.flab.fream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreamApplication.class, args);
+	}
+
+}

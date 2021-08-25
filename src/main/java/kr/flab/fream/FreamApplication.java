e7603377f5d8fc6f6bcf9302eca2eda9633361dc
Jake <cocahack@naver.com>
2021-08-25 13:25:22 +0900

    test: testcontainers 사용하여 로컬 테스트 환경 구축

--- src/main/java/kr/flab/fream/FreamApplication.java
+++ src/main/java/kr/flab/fream/FreamApplication.java
@@ -3,11 +3,14 @@ package kr.flab.fream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Starting point of an application.
+ */
 @SpringBootApplication
 public class FreamApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FreamApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FreamApplication.class, args);
+    }
 
 }

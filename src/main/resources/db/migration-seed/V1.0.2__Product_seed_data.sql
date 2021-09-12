93717424df37f22bada33f628c33b241151e6de5
Jake <cocahack@naver.com>
2021-09-12 13:10:13 +0900

    feat: product seed data 추가

---
+++ src/main/resources/db/migration-seed/V1.0.2__Product_seed_data.sql
@@ -0,0 +1,341 @@
+# Nike
+
+INSERT INTO product(id, name, english_name, product_code, release_date, retail_price, brand_id,
+                    category)
+VALUES (1, '나이키 덩크 로우 레트로 블랙', 'Nike Dunk Low Retro Black', 'DD1391-100', '2021-01-14', 119000, 1,
+        'SNEAKERS');
+
+INSERT INTO product_size(product_id, size_id)
+VALUES (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 4),
+       (1, 5),
+       (1, 6),
+       (1, 7),
+       (1, 8),
+       (1, 9),
+       (1, 10),
+       (1, 11),
+       (1, 12),
+       (1, 13),
+       (1, 14),
+       (1, 15),
+       (1, 16),
+       (1, 17),
+       (1, 18),
+       (1, 19);
+
+
+INSERT INTO product(id, name, english_name, product_code, release_date, retail_price, brand_id,
+                    category)
+VALUES (2, '나이키 x 오프화이트 덩크 로우 더 50 - 로트 18', 'Nike x Off-White Dunk Low The 50 - Lot 18',
+        'DJ0950-112', '2021-08-09', 219000, 1, 'SNEAKERS');
+
+INSERT INTO product_size(product_id, size_id)
+VALUES (2, 1),
+       (2, 2),
+       (2, 3),
+       (2, 4),
+       (2, 5),
+       (2, 6),
+       (2, 7),
+       (2, 8),
+       (2, 9),
+       (2, 10),
+       (2, 11),
+       (2, 12),
+       (2, 13),
+       (2, 14),
+       (2, 15),
+       (2, 16),
+       (2, 17),
+       (2, 18),
+       (2, 19);
+
+
+INSERT INTO product(id, name, english_name, product_code, release_date, retail_price, brand_id,
+                    category)
+VALUES (3, '나이키 x 피스마이너스원 에어포스 1 로우 파라노이즈 2.0',
+        'Nike x Peaceminusone Air Force 1 Low Para-Noise 2.0', 'DD3223-100', '2020-11-25', 219000,
+        1, 'SNEAKERS');
+
+INSERT INTO product_size(product_id, size_id)
+VALUES (3, 1),
+       (3, 2),
+       (3, 3),
+       (3, 4),
+       (3, 5),
+       (3, 6),
+       (3, 7),
+       (3, 8),
+       (3, 9),
+       (3, 10),
+       (3, 11),
+       (3, 12),
+       (3, 13),
+       (3, 14),
+       (3, 15),
+       (3, 16),
+       (3, 17),
+       (3, 18),
+       (3, 19);
+
+
+INSERT INTO product(id, name, english_name, product_code, release_date, retail_price, brand_id,
+                    category)
+VALUES (4, '나이키 x 스투시 윈드러너 자켓 오프 느와르', 'Nike x Stussy Windrunner Jacket Off Noir', 'CT4310-045',
+        null, 229000, 1, 'OUTER');
+
+INSERT INTO product_size(product_id, size_id)
+VALUES (4, 20),
+       (4, 21),
+       (4, 22),
+       (4, 23),
+       (4, 24),
+       (4, 25)
+;
+
+INSERT INTO product(id, name, english_name, product_code, release_date, retail_price, brand_id,
+                    category)
+VALUES (5, '나이키 x 스투시 비치팬츠 오프 느와르', 'Nike x Stussy Beach Pants Off Noir', 'CT4316-045', null,
+        169000, 1, 'BOTTOM');
+
+INSERT INTO product_size(product_id, size_id)
+VALUES (5, 20),
+       (5, 21),
+       (5, 22),
+       (5, 23),
+       (5, 24),
+       (5, 25)
+;
+
+
+INSERT INTO product(id, name, english_name, product_code, release_date, retail_price, brand_id,
+                    category)
+VALUES (6, '나이키 x 오프화이트 NRG 팬츠 블랙', 'Nike x Off-White NRG Pants Black', 'CU2500-010', '2021-07-23',
+        279000, 1, 'BOTTOM');
+
+INSERT INTO product_size(product_id, size_id)
+VALUES (6, 20),
+       (6, 21),
+       (6, 22),
+       (6, 23),
+       (6, 24),
+       (6, 25)
+;
+
+
+# Adidas
+
+INSERT INTO product(id, name, english_name, product_code, release_date, retail_price, brand_id,
+                    category)
+VALUES (51, '아디다스 이지 부스트 350 V2 블랙 레드 2020', 'Adidas Yeezy Boost 350 V2 Black Red 2020', 'CP9652',
+        '2020-12-05', 289000, 2, 'SNEAKERS');
+
+INSERT INTO product_size(product_id, size_id)
+VALUES (51, 1),
+       (51, 2),
+       (51, 3),
+       (51, 4),
+       (51, 5),
+       (51, 6),
+       (51, 7),
+       (51, 8),
+       (51, 9),
+       (51, 10),
+       (51, 11),
+       (51, 12),
+       (51, 13),
+       (51, 14),
+       (51, 15),
+       (51, 16),
+       (51, 17),
+       (51, 18),
+       (51, 19);
+
+
+INSERT INTO product(id, name, english_name, product_code, release_date, retail_price, brand_id,
+                    category)
+VALUES (52, '아디다스 이지 슬라이드 퓨어', 'Adidas Yeezy Slide Pure', 'GZ5554', '2021-04-26', 289000, 2,
+        'SNEAKERS');
+
+INSERT INTO product_size(product_id, size_id)
+VALUES (52, 1),
+       (52, 2),
+       (52, 3),
+       (52, 4),
+       (52, 5),
+       (52, 6),
+       (52, 7),
+       (52, 8),
+       (52, 9),
+       (52, 10),
+       (52, 11),
+       (52, 12),
+       (52, 13),
+       (52, 14),
+       (52, 15),
+       (52, 16),
+       (52, 17),
+       (52, 18),
+       (52, 19);
+
+
+INSERT INTO product(id, name, english_name, product_code, release_date, retail_price, brand_id,
+                    category)
+VALUES (53, '아디다스 슈퍼스타 프리미엄 화이트 블랙', 'Adidas Superstar Premium White Black', 'FV2831', '2020-01-01',
+        116600, 2, 'SNEAKERS');
+
+INSERT INTO product_size(product_id, size_id)
+VALUES (53, 1),
+       (53, 2),
+       (53, 3),
+       (53, 4),
+       (53, 5),
+       (53, 6),
+       (53, 7),
+       (53, 8),
+       (53, 9),
+       (53, 10),
+       (53, 11),
+       (53, 12),
+       (53, 13),
+       (53, 14),
+       (53, 15),
+       (53, 16),
+       (53, 17),
+       (53, 18),
+       (53, 19);
+
+
+# New Balance Sneakers
+
+INSERT INTO product(id, name, english_name, product_code, release_date, retail_price, brand_id,
+                    category)
+VALUES (101, '뉴발란스 992 메이드 인 USA 그레이 (D 스탠다드)', 'New Balance 992 Made in USA Grey (D Standard)',
+        'M992GR', '2020-04-13', 259000, 3, 'SNEAKERS');
+
+INSERT INTO product_size(product_id, size_id)
+VALUES (101, 1),
+       (101, 2),
+       (101, 3),
+       (101, 4),
+       (101, 5),
+       (101, 6),
+       (101, 7),
+       (101, 8),
+       (101, 9),
+       (101, 10),
+       (101, 11),
+       (101, 12),
+       (101, 13),
+       (101, 14),
+       (101, 15),
+       (101, 16),
+       (101, 17),
+       (101, 18),
+       (101, 19);
+
+
+# Jordan
+INSERT INTO product(id, name, english_name, product_code, release_date, retail_price, brand_id,
+                    category)
+VALUES (151, '조던 1 x 트래비스 스캇 레트로 하이 OG SP 모카', 'Jordan 1 x Travis Scott Retro High OG SP Mocha',
+        'CD4487-100', '2019-05-12', 239000, 5, 'SNEAKERS');
+
+INSERT INTO product_size(product_id, size_id)
+VALUES (151, 1),
+       (151, 2),
+       (151, 3),
+       (151, 4),
+       (151, 5),
+       (151, 6),
+       (151, 7),
+       (151, 8),
+       (151, 9),
+       (151, 10),
+       (151, 11),
+       (151, 12),
+       (151, 13),
+       (151, 14),
+       (151, 15),
+       (151, 16),
+       (151, 17),
+       (151, 18),
+       (151, 19);
+
+
+INSERT INTO product(id, name, english_name, product_code, release_date, retail_price, brand_id,
+                    category)
+VALUES (152, '조던 1 x 트래비스 스캇 x 프라그먼트 레트로 하이 OG SP 밀리터리 블루',
+        'Jordan 1 x Travis Scott x Fragment Retro High OG SP Military Blue',
+        'DH3227-105', '2021-07-29', 239000, 5, 'SNEAKERS');
+
+INSERT INTO product_size(product_id, size_id)
+VALUES (152, 1),
+       (152, 2),
+       (152, 3),
+       (152, 4),
+       (152, 5),
+       (152, 6),
+       (152, 7),
+       (152, 8),
+       (152, 9),
+       (152, 10),
+       (152, 11),
+       (152, 12),
+       (152, 13),
+       (152, 14),
+       (152, 15),
+       (152, 16),
+       (152, 17),
+       (152, 18),
+       (152, 19);
+
+# Supreme
+INSERT INTO product(id, name, english_name, product_code, release_date, retail_price, brand_id,
+                    category)
+VALUES (201, '슈프림 워시드 치노 트윌 캠프캡 블랙', 'Supreme Washed Chino Twill Camp Cap Black',
+        null, null, 55900, 6, 'HAT');
+
+INSERT INTO product_size(product_id, size_id)
+VALUES (201, 30)
+;
+
+
+INSERT INTO product(id, name, english_name, product_code, release_date, retail_price, brand_id,
+                    category)
+VALUES (202, '슈프림 크로스 박스로고 후드 스웨트셔츠 블랙 (20FW)',
+        'Supreme Cross Box Logo Hooded Sweatshirt Black (20FW)', null, '2020-12-03', 195900, 6,
+        'TOP');
+
+INSERT INTO product_size(product_id, size_id)
+VALUES (202, 21),
+       (202, 22),
+       (202, 23),
+       (202, 24)
+;
+
+
+# Sony
+INSERT INTO product(id, name, english_name, product_code, release_date, retail_price, brand_id,
+                    category)
+VALUES (251, '소니 플레이스테이션 5 디지털 에디션 (한국 정식 발매 제품)', 'Sony Playstation 5 Digital Edition (SIEK 220V)',
+        null, '2020-11-12', 498000, 10, 'GAMING_CONSOLE');
+
+INSERT INTO product_size(product_id, size_id)
+VALUES (251, 30)
+;
+
+
+# Lego
+
+INSERT INTO product(id, name, english_name, product_code, release_date, retail_price, brand_id,
+                    category)
+VALUES (301, '레고 DC 코믹스 배트맨 텀블러', 'Lego DC Comics Batman The Tumbler',
+        '76023', '2014-12-24', 233200, 14, 'LIFE');
+
+INSERT INTO product_size(product_id, size_id)
+VALUES (301, 30)
+;

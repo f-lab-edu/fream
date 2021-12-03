89a09e3eee524921f46fc5a79b8ee5caca904561
Jake <cocahack@naver.com>
2021-12-03 22:52:30 +0900

    chore: auction seed 데이터 추가

---
+++ src/main/resources/db/migration-seed/V1.3.2__Auction_seed_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO auction (id, price, product_id, size_id, due_date, type, user_id)
+VALUES (1, 100000, 1, 1, '2099-12-31 23:59:59', 'ASK', 1);
+
+INSERT INTO auction (id, price, product_id, size_id, due_date, type, user_id)
+VALUES (2, 150000, 1, 1, '2099-12-31 23:59:59', 'BID', 1);

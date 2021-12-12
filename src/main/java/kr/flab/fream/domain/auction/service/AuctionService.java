7062817001b8eeb303b6bb37c7bb43f4cb70ca71
Jake <cocahack@naver.com>
2021-12-12 22:53:20 +0900

    feat: 입찰 요약 목록 조회 컨트롤러 및 서비스 구현

--- src/main/java/kr/flab/fream/domain/auction/service/AuctionService.java
+++ src/main/java/kr/flab/fream/domain/auction/service/AuctionService.java
@@ -1,9 +1,14 @@
 package kr.flab.fream.domain.auction.service;
 
+import java.math.BigDecimal;
+import java.util.List;
+import javax.annotation.Nullable;
 import kr.flab.fream.controller.auction.AuctionDto;
 import kr.flab.fream.controller.auction.AuctionPatchRequest;
 import kr.flab.fream.controller.auction.AuctionRequest;
+import kr.flab.fream.controller.auction.AuctionSummaryByPriceAndSizeWithQuantity;
 import kr.flab.fream.domain.auction.model.Auction;
+import kr.flab.fream.domain.auction.model.AuctionType;
 import kr.flab.fream.domain.product.model.Product;
 import kr.flab.fream.domain.product.model.Size;
 import kr.flab.fream.domain.product.service.ProductService;
@@ -29,6 +34,20 @@ public class AuctionService {
     private final UserMapper userMapper;
     private final ProductService productService;
     private final ModelMapper modelMapper;
+
+    /**
+     * 구매 혹은 판매 입찰의 가격별 수량 조회.
+     *
+     * @param type      입찰 타입
+     * @param productId 조회할 상품 번호
+     * @param sizeId    조회할 상품의 특정 사이즈 번호
+     * @param lastPrice 마지막으로 조회했던 가격
+     * @return 사이즈별 가격과 수량을 반환
+     */
+    public List<AuctionSummaryByPriceAndSizeWithQuantity> getAuctionSummaries(AuctionType type,
+            Long productId, @Nullable Long sizeId, @Nullable BigDecimal lastPrice) {
+        return auctionMapper.getAuctionSummaries(type, productId, sizeId, lastPrice);
+    }
 
     /**
      * 구매 입찰을 생성한다.

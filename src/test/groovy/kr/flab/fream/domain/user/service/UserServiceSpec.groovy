137b94e462af4a75240cbfd7194d6732b7b5cfff
HyundeokPark <rphd0923@gmail.com>
2021-12-15 17:27:51 +0900

    wip : 로그인 서비스 유닛테스트 작성중

--- src/test/groovy/kr/flab/fream/domain/user/service/UserServiceSpec.groovy
+++ src/test/groovy/kr/flab/fream/domain/user/service/UserServiceSpec.groovy
@@ -1,6 +1,7 @@
 package kr.flab.fream.domain.user.service
 
 import kr.flab.fream.config.ModelMapperConfiguration
+import kr.flab.fream.controller.user.UserDto
 import kr.flab.fream.domain.user.model.User
 import kr.flab.fream.mybatis.mapper.user.UserMapper
 import org.modelmapper.ModelMapper
@@ -10,19 +11,29 @@ import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
 
 class UserServiceSpec extends Specification {
-
-
-
     def "get user"() {
         given :
         def modelMapper = new ModelMapperConfiguration().modelMapper();
 
         UserMapper userMapper = Stub() {
-            getUser(_ as Long) >> { Long id -> new User(id) }
+            getUserById(_ as Long) >> { Long id -> new User(id) }
         }
 
         def userService = new UserService(userMapper, modelMapper)
         expect:
-        userService.getUser(1L).getId()==1
+        userService.getUserById(1L).getId()==1
+    }
+    def"user login"(){
+        given :
+        def modelMapper = new ModelMapperConfiguration().modelMapper();
+
+        UserMapper userMapper = Stub() {
+            getUser(_ as UserDto) >> { String email,String password -> new UserDto(email,password) }
+        }
+        def userDto = new UserDto("test@test.com","1234");
+        def userService = new UserService(userMapper, modelMapper)
+
+        expect:
+        userService.userLogin(userDto).getId()==1
     }
 }

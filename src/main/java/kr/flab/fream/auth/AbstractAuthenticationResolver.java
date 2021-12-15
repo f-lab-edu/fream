2f548de1be7bdb23ab4f7b54d3bdb154366d3e40
Jake <cocahack@naver.com>
2021-12-15 21:31:27 +0900

    feat: 인증 정보를 컨트롤러에 주입하는 기능 추가 (#39)
    * feat: 인증 정보를 컨트롤러에 주입하는 기능 추가
    
    * refactor: AuthenticationInterceptor 제거
    
    인터셉터가 구현되는지는 알 수 없기 때문에
    HandlerMethodArgumentResolver 만 사용하기로 한다.

---
+++ src/main/java/kr/flab/fream/auth/AbstractAuthenticationResolver.java
@@ -0,0 +1,19 @@
+package kr.flab.fream.auth;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+
+/**
+ * {@code @Authentication} 을 사용할 때 인증한 유저를 컨트롤러에 주입해주는 resolver 의 추상 클래스.
+ *
+ * @since 1.0.0
+ * @author Jake
+ */
+public abstract class AbstractAuthenticationResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public final boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(Authentication.class);
+    }
+
+}
---
+++ src/main/java/kr/flab/fream/config/WebConfig.java
@@ -0,0 +1,22 @@
+package kr.flab.fream.config;
+
+import java.util.List;
+import kr.flab.fream.auth.AbstractAuthenticationResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 인터셉터와 MethodArgumentResolver 등록.
+ */
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AbstractAuthenticationResolver authenticationResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authenticationResolver);
+    }
+
+}

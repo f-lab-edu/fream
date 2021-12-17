ef7c9bd79ad63d9cef0fe3d2e2f1acba4fcb5511
ellieTest <hpotter1993@naver.com>
2021-12-17 23:49:15 +0900

    refactor : 컨트롤러 api 주소 변경 및 userDto 반환으로 변경

--- src/main/java/kr/flab/fream/controller/user/UserController.java
+++ src/main/java/kr/flab/fream/controller/user/UserController.java
@@ -1,7 +1,9 @@
 package kr.flab.fream.controller.user;
 
+import java.net.http.HttpRequest;
 import java.util.List;
 import javax.servlet.http.HttpSession;
+import javax.validation.constraints.NotNull;
 import kr.flab.fream.controller.product.ProductDto;
 import kr.flab.fream.domain.product.OrderOption;
 import kr.flab.fream.domain.product.OrderOption.OrderOptionConverter;
@@ -28,16 +30,17 @@ public class UserController {
     private final UserService userService;
     private final ModelMapper modelMapper;
 
-    @RequestMapping(value = "/login")
-    public Boolean login(HttpSession session){
+    @RequestMapping(value = "/login/{email}/{password}")
+    public UserDto login(HttpSession session,
+        @NotNull  @PathVariable String email
+        ,@NotNull @PathVariable String password){
 
-        UserDto userDto = new UserDto("hpotter300@naver.com","12345678");
-        Boolean resultCd = false;
-        if(!ObjectUtils.isEmpty(userService.userLogin(userDto))){
-            session.setAttribute("userInfo",userService.userLogin(userDto));
-            resultCd = true;
+        UserDto userDto = new UserDto(email,password);
+        UserDto userInfo = userService.userLogin(userDto);
+        if(!ObjectUtils.isEmpty(userInfo)){
+            session.setAttribute("userInfo",userInfo);
         }
-        return resultCd;
+        return userInfo;
     }
 
 }

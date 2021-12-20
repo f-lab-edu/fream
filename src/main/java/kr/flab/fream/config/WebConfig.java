b2daf9155e6b8dab735a36d27e5ee42a26e6bb66
HyundeokPark <hpotter1993@naver.com>
2021-12-21 00:08:43 +0900

    feat : LoginInterceptor 적용

--- src/main/java/kr/flab/fream/config/WebConfig.java
+++ src/main/java/kr/flab/fream/config/WebConfig.java
@@ -18,10 +18,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final AbstractAuthenticationResolver authenticationResolver;
+    private final LoginInterceptor loginInterceptor;
+    private static final List<String> URL_PATTERNS = Arrays.asList("/user/**");  //인터셉터가 동작 해야 될 요청 주소
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authenticationResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+            .addPathPatterns(URL_PATTERNS)
+            .excludePathPatterns("/user/test/**");
     }
 
 }
--- src/main/java/kr/flab/fream/interceptor/LoginInterceptor.java
+++ src/main/java/kr/flab/fream/interceptor/LoginInterceptor.java
@@ -20,21 +20,12 @@ public class LoginInterceptor implements HandlerInterceptor {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-        ModelAndView modelAndView) throws Exception {
-        logger.debug("post call");
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
-    }
-
-    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
         Object handler) throws Exception {
-
         logger.debug("intercepts");
         if(ObjectUtils.isEmpty(request.getSession().getAttribute("userInfo"))){
             return false;
         }
         return false;
-        //return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 }

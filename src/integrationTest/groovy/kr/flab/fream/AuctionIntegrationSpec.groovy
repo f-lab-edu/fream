e3fbd25a3432619291a68bec36be6f1100d9399f
HyundeokPark <hpotter1993@naver.com>
2022-01-04 00:16:05 +0900

    wip : integration Test 작성중...

--- src/integrationTest/groovy/kr/flab/fream/AuctionIntegrationSpec.groovy
+++ src/integrationTest/groovy/kr/flab/fream/AuctionIntegrationSpec.groovy
@@ -1,18 +1,26 @@
 package kr.flab.fream
 
 import io.restassured.http.ContentType
+import kr.flab.fream.controller.user.LoginDto
+import kr.flab.fream.controller.user.UserDto
 import kr.flab.fream.domain.auction.model.AuctionType
+import kr.flab.fream.domain.user.service.UserService
 import kr.flab.fream.mybatis.mapper.auction.AuctionMapper
 import kr.flab.fream.mybatis.mapper.product.ProductMapper
 import kr.flab.fream.mybatis.mapper.user.AddressMapper
 import kr.flab.fream.mybatis.mapper.user.UserMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.http.HttpRequest
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.mock.web.MockHttpSession
+import org.springframework.test.web.servlet.MockMvc
 
 import static io.restassured.RestAssured.given
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 
 class AuctionIntegrationSpec extends BaseIntegrationSpec {
 
@@ -31,17 +39,19 @@ class AuctionIntegrationSpec extends BaseIntegrationSpec {
     @Autowired
     AddressMapper addressMapper
 
+
     def "create #type"() {
         given:
-       def session = new MockHttpSession();
-       session.setAttribute("userInfo","test")
+        def session = new MockHttpSession();
+        session.setAttribute("userInfo", "dd")
         def request = given(this.spec).contentType(ContentType.JSON).body(body)
             .filter(document(identifier))
             .log()
             .all()
 
         when:
-        def response = request.when().port(this.port).post("/" + identifier)
+        def response = request.when().sessionId(session.getId()).port(this.port).post("/" + identifier)
+
 
         then:
         response.getStatusCode() == HttpStatus.CREATED.value()

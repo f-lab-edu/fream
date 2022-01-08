14de874c61d81c2866bec7648d1d74336fa2821d
HyundeokPark <hpotter1993@naver.com>
2022-01-09 00:38:27 +0900

    doc : 암/복호화 인터페이스, 구현체 javadoc 설명 추가

--- src/main/java/kr/flab/fream/util/EncryptHelper.java
+++ src/main/java/kr/flab/fream/util/EncryptHelper.java
@@ -2,7 +2,9 @@ package kr.flab.fream.util;
 
 import org.springframework.stereotype.Component;
 
-
+/**
+ * 암복호화를 도와주는 인터페이스 라이브러리, 사용방식등이 달라 질수 있기에 인터페이스로 선언함.
+ */
 public interface EncryptHelper {
     String encryptPassword(String password);
 }

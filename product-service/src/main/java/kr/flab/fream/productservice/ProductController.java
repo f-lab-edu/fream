c3ee271576eafa9b9f080f8728642ced7e406cef
Jake <cocahack@naver.com>
2022-02-12 14:50:39 +0900

    feat: 컨트롤러 추가

---
+++ product-service/src/main/java/kr/flab/fream/productservice/ProductController.java
@@ -0,0 +1,16 @@
+package kr.flab.fream.productservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping("/")
+    public String get() {
+        return "Sneakers";
+    }
+
+}
